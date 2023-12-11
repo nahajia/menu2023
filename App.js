7570e71ffd1627cefc1821489a04a72497c9582b
Aniko <nahajia@gmail.com>
2023-12-11 15:39:06 +0100

    2023_12_11

--- App.js
+++ App.js
@@ -2,15 +2,6 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Proba from './Proba';
-import Mogyoro from "./Mogyoro"
-import Repa from './Repa';
-import Retek from "./Retek"
-import Kepfeltoltes from "./Kepfeltoltes"
-import Felvitel from "./Felvitel"
-import Lenyiloteszt from "./Lenyiloteszt"
-import Lenyilo from "./Lenyilo"
 
 function HomeScreen({ navigation }) {
   return (
@@ -18,10 +9,6 @@ function HomeScreen({ navigation }) {
       <Button
         onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
-      />
-       <Button
-        onPress={() => navigation.navigate('Proba')}
-        title="Próba screen meghívása"
       />
     </View>
   );
@@ -34,36 +21,16 @@ function NotificationsScreen({ navigation }) {
     </View>
   );
 }
-function Root({ navigation }) {
-  return (
-    <Drawer.Navigator initialRouteName="Home">
-    <Drawer.Screen name="Home" component={HomeScreen} />
-    <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-    <Drawer.Screen name="Mogyoro" component={Mogyoro} />
-    <Drawer.Screen name="Kepfeltotles" component={Kepfeltoltes} />  
-    <Drawer.Screen name="Felvitel" component={Felvitel} />     
-    <Drawer.Screen name="Lenyiloteszt" component={Lenyiloteszt} /> 
-    <Drawer.Screen name="Lenyilo" component={Lenyilo} /> 
-    </Drawer.Navigator>
-
-  );
-}
-
-
-
 
 const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen name="Root" component={Root} options={{headerShown:false}} />
-        <Stack.Screen name="Proba" component={Proba}  />  
-        <Stack.Screen name="Repa" component={Repa}  />       
-        <Stack.Screen name="Retek" component={Retek}  />       
-      </Stack.Navigator>     
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- Ipcim.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    Ipcim: 'http://10.0.0.235:3000/'
-    //Ipcim: 'http://10.0.0.235:3000/' 
-  };
-  
